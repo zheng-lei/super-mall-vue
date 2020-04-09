9e22c96d195c4606e47a19549563f5889109253e
zhenglei <zhenglei@leofs.com.cn>
2020-04-09 13:46:49 +0800

    fix@配置

---
+++ src/vue.config.js
@@ -0,0 +1,13 @@
+moudule.exports = {
+    configureWebpack: {
+        resolve: {
+            alias: {
+                "assets": "@/assets",
+                "common": "@/common",
+                "components": "@components",
+                "network": "@/network",
+                "views": "@/views"
+            }
+        }
+    }
+}
